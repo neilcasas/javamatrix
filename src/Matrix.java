a198709a4f5c3e9defebe882bd17f1b833f67cf1
Neil Casas <neilalfonzcasas@gmail.com>
2024-01-27 18:49:33 +0800

    Added constructor methods

--- src/Matrix.java
+++ src/Matrix.java
@@ -1,7 +1,20 @@
 public class Matrix {
+    private final int max = 20;
     private int rows;
     private int columns;
     private int[][] matrix;
 
+    // Constructor
+    public Matrix(int rows, int columns){
+        if(rows <= max && columns <= max){
+            matrix = new int[rows][columns];
+        } else {
+            throw new IllegalArgumentException("Invalid matrix dimension.");
+        }
+    }
+    public Matrix() {
+        matrix = new int[3][3];
+    }
 
+    
 }
